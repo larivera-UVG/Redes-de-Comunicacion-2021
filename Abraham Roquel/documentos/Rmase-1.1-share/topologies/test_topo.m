e35234e56b57660736b8205178b407bcb8190e9a
ERICK ABRAHAM ROQUEL PISQUIY <roq17529@uvg.edu.gt>
2021-10-07 01:04:28 -0600

    AntNet: prueba de tiny OS
    prueba de la funcionalidad general de prowler y su implementación de TinyOS

--- Abraham Roquel/documentos/Rmase-1.1-share/topologies/test_topo.m
+++ Abraham Roquel/documentos/Rmase-1.1-share/topologies/test_topo.m
@@ -1,6 +1,6 @@
 function [topology, mote_IDs] = test_topo(varargin)
 	mote_IDs = [1, 2, 3];	%definir los IDs de los nodos
 	mote_Xs = [0, 0, 0];	%ubicaciones respectivas sobre eje X
-	mote_Ys = [0, 1, 7];	%ubicaciones respectivas sobre eje Y
+	mote_Ys = [0, 1, 2];	%ubicaciones respectivas sobre eje Y
 	topology = [mote_Xs', mote_Ys'];
 end
